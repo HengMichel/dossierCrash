a4de43f9bb6da15053993c5f502a8afda0b32b7a
heng michel <cavacmoi@gmail.com>
2023-11-07 16:55:45 +0100

    secondCommitBis

--- public/footer.html.php
+++ public/footer.html.php
@@ -1,4 +1,9 @@
 <p>footer</p>
+<div class="pied d-felx link-danger">
+<i class="fa-brands fa-facebook fa-beat-fade"></i>
+<i class="fa-brands fa-square-instagram fa-beat-fade"></i>
+<i class="fa-brands fa-twitter fa-beat-fade"></i>
+</div>
 </div>
 </div>
 </body>
