b83beb0ff691e958787846f5063f0a6c25107df2
cavacmoi <cavacmoi@gmail.com>
2023-11-07 21:20:40 +0100

    addFooter

--- public/footer.html.php
+++ public/footer.html.php
@@ -1,9 +1,10 @@
 <p>footer</p>
-<div class="pied d-felx link-danger">
-<i class="fa-brands fa-facebook fa-beat-fade"></i>
-<i class="fa-brands fa-square-instagram fa-beat-fade"></i>
-<i class="fa-brands fa-twitter fa-beat-fade"></i>
-</div>
+<div class="pied d-felx fa-2x modal-dialog">
+<i class="fa-brands fa-square-snapchat fa-spin fa-spin-reverse link-info"></i>
+<i class="fa-brands fa-facebook fa-beat-fade link-secondary"></i>
+<i class="fa-brands fa-tiktok fa-shake link-warning"></i>
+<i class="fa-brands fa-twitter fa-beat-fade link-primary"></i>
+<i class="fa-brands fa-square-instagram fa-spin link-danger"></i></div>
 </div>
 </div>
 </body>
